ca408194cbccd89edcd9c78a7810535a921a4497
Laiender Camargos <laiender.morais@gmail.com>
2026-06-13 14:13:11 -0300

    Continuacao da implementação.

--- adapter/http/main.go
+++ adapter/http/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/laienderCamargos/Planejamento_Financeiro_Go/adapter/http/actuator"
 	"github.com/laienderCamargos/Planejamento_Financeiro_Go/adapter/http/transaction"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // init
@@ -13,6 +14,8 @@ func Init() {
 	http.HandleFunc("/transaction/create", transaction.CreateTransaction)
 
 	http.HandleFunc("/health", actuator.Health)
+
+	http.Handle("/metrics", promhttp.Handler())
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
